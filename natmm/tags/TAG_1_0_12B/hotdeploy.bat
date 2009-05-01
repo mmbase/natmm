1d7d194fe748cbeb7be2395c3b3e467066740e1d
nico <nico@91430c5f-d668-0410-9e7d-dd663366dcfd>
2009-05-01 15:28:01 +0000

    move TAG_1_0_12B

---
+++ natmm/tags/TAG_1_0_12B/hotdeploy.bat
@@ -0,0 +1,23 @@
+@echo off
+
+IF exist "%CATALINA_HOME%\webapps\natmm-internet\index.jsp" GOTO internet
+IF exist "%CATALINA_HOME%\webapps\ROOT\nmintra\info.jsp" GOTO intranet
+GOTO nothing
+
+:internet
+echo Copying templates\natmm to %CATALINA_HOME%\webapps\natmm-internet
+xcopy templates\natmm\*.js* %CATALINA_HOME%\webapps\natmm-internet  /S /D /Y
+xcopy templates\natmm\*.htm* %CATALINA_HOME%\webapps\natmm-internet  /S /D /Y
+GOTO end
+
+:intranet
+echo Copying templates\nmintra to %CATALINA_HOME%\webapps\ROOT\nmintra
+xcopy templates\nmintra\*.js* %CATALINA_HOME%\webapps\ROOT\nmintra  /S /D /Y
+xcopy templates\nmintra\*.htm* %CATALINA_HOME%\webapps\ROOT\nmintra  /S /D /Y
+GOTO end
+
+:nothing
+echo Nothing good found in %CATALINA_HOME%\webapps !!
+
+:end
+echo Done.
---
+++ natmm/tags/TAG_1_0_12B/templates/nmintra/update/sql_scripts/08.10.02.migrate.sql
@@ -0,0 +1,1 @@
+ALTER TABLE v1_thema_plot_kaart ADD positie1 int(11);
