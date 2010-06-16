8b9ae7dfee4897ecd38746d2cc3d88a408fd47e2
jkoster <jkoster@91430c5f-d668-0410-9e7d-dd663366dcfd>
2010-06-16 14:38:15 +0000

    Internet-release 1.19 voor NMCMS-696.

---
+++ natmm/tags/TAG_1_19/onlyjar.bat
@@ -0,0 +1,2 @@
+rem C:\SERVER\ANT\bin\ant.bat natmmjar > build.log
+C:\apps\apache-ant-1.6.2\bin\ant.bat natmmjar > build.log
---
+++ natmm/tags/TAG_1_19/templates/natmm/update/sql_scripts/NMCMS_573_delete_inschrijvingen.sql
@@ -0,0 +1,14 @@
+delete from v1_object where number = 246381;
+delete from v1_insrel where number = 246381;
+delete from v1_object where number = 261634;
+delete from v1_insrel where number = 261634;
+delete from v1_object where number = 520124;
+delete from v1_insrel where number = 520124;
+delete from v1_object where number = 547047;
+delete from v1_insrel where number = 547047;
+delete from v1_object where number = 547047;
+delete from v1_insrel where number = 547047;
+delete from v1_object where number = 1199590;
+delete from v1_insrel where number = 1199590;
+delete from v1_object where number = 2206964;
+delete from v1_insrel where number = 2206964;
